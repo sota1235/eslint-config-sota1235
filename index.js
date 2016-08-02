c56b19fd831b54092c36861eb5331d5bc7f95d41
sota1235 <sota1235@gmail.com>
2016-08-02 23:43:21 +0900

    :sparkles: add rules for me

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 module.exports = {
   extends: [
+    'airbnb',
   ].map(require.resolve),
-  rules: {}
+  rules: {
+    "no-unused-vars": ["warn"],
+    "no-multi-spaces": [
+      "error",
+      {
+        "exceptions": {
+          "VariableDeclarator": true,
+          "ImportDeclaration": true
+        }
+      }
+    ],
+    "no-console": ["off"],
+    "key-spacing": ["error", {
+      "align": {
+        "beforeColon": true,
+        "afterColon": true,
+        "on": "colon"
+      }
+    }],
+    "react/jsx-boolean-value": ["off"],
+    "react/prop-types": ["warn"]
+  }
 };
