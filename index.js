4927487941748bf7753ad051e99bb78e3b9c7cfb
sota1235 <sota1235@gmail.com>
2016-08-03 00:00:39 +0900

    :recycle: refactor with npm run refacto

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 module.exports = {
-  extends: 'airbnb',
-  rules: {
-    "no-unused-vars": ["warn"],
-    "no-multi-spaces": [
-      "error",
+  extends : 'airbnb',
+  rules   : {
+    'no-unused-vars'  : ['warn'],
+    'no-multi-spaces' : [
+      'error',
       {
-        "exceptions": {
-          "VariableDeclarator": true,
-          "ImportDeclaration": true
-        }
-      }
+        'exceptions': {
+          'VariableDeclarator' : true,
+          'ImportDeclaration'  : true,
+        },
+      },
     ],
-    "no-console": ["off"],
-    "key-spacing": ["error", {
-      "align": {
-        "beforeColon": true,
-        "afterColon": true,
-        "on": "colon"
-      }
+    'no-console'  : ['off'],
+    'key-spacing' : ['error', {
+      'align': {
+        'beforeColon' : true,
+        'afterColon'  : true,
+        'on'          : 'colon',
+      },
     }],
-    "react/jsx-boolean-value": ["off"],
-    "react/prop-types": ["warn"]
-  }
+    'react/jsx-boolean-value' : ['off'],
+    'react/prop-types'        : ['warn'],
+  },
 };
